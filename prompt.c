6ba9444061c71ef0394d9765d1ead247d666d06d
oscarcederberg <os7138ce-s@student.lu.se>
2024-06-19 22:06:44 +0200

    Finish ch. 7 "Evaluation"

--- prompt.c
+++ prompt.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <editline/readline.h>
 #include <editline/history.h>
 #include <stdbool.h>
@@ -12,6 +13,45 @@ typedef struct language_t {
     mpc_parser_t *expr;
     mpc_parser_t *lispy;
 } language_t;
+
+long ast_eval_operator (long left, char * operator, long right) {
+    long value;
+
+    if (strcmp (operator, "+") == 0) {
+        value = left + right;
+    } else if (strcmp (operator, "-") == 0) {
+        value = left - right;
+    } else if (strcmp (operator, "*") == 0) {
+        value = left * right;
+    } else if (strcmp (operator, "/") == 0) {
+        value = left / right;
+    } else {
+        assert (false);
+    }
+
+    return value;
+}
+
+long ast_eval (mpc_ast_t * ast) {
+    char *operator;
+    long value;
+
+    assert (ast != NULL);
+
+    if (strstr (ast->tag, "number")) {
+        return atoi (ast->contents);
+    }
+
+    operator = ast->children[1]->contents;
+
+    value = ast_eval (ast->children[2]);
+
+    for (size_t i = 3; strstr (ast->children[i]->tag, "expr"); i++) {
+        value = ast_eval_operator (value, operator, ast_eval (ast->children[i]));
+    }
+
+    return value;
+}
 
 bool language_parse (language_t * language, const char * input, mpc_result_t *
         result) {
@@ -39,6 +79,8 @@ language_t * language_new (void) {
 }
 
 void language_free (language_t * language) {
+    assert (language != NULL);
+
     mpc_cleanup (
         4, language->number, language->operator, language->expr, language->lispy
     );
@@ -55,9 +97,11 @@ int main (int argc, char ** argv) {
     while (1) {
         char *input = readline ("lispy> ");
         mpc_result_t result;
+        long value;
 
         if (language_parse (language, input, &result)) {
-            mpc_ast_print (result.output);
+            value = ast_eval (result.output);
+            printf ("%li\n", value);
             mpc_ast_delete (result.output);
         } else {
             mpc_err_print (result.error);
